dcecfdb2dc80784131033878a2934a67d590028b
jguimeradev <jguimeradev@gmail.com>
2026-02-02 06:02:50 +0100

    02: date format insteado of string function

--- 02/main.go
+++ 02/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	"time"
 )
 
 var (
@@ -18,31 +18,15 @@ var (
 	White   = "\033[97m"
 )
 
-func formatModTime(modTime string) string {
-
-	m := strings.Split(modTime, " ")
-
-	b, _, _ := strings.Cut(m[1], ".")
-
-	m[1] = b
-
-	modified := strings.Join(m[:2], " ")
-
-	return modified
-}
-
 func main() {
 
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: ./main ls <args>")
+	if len(os.Args) < 3 || len(os.Args) > 3 {
+		fmt.Println(len(os.Args))
+		fmt.Println("Usage: ./main ls <path>")
 		os.Exit(1)
 	}
 
-	var path string
-
-	//TODO: fix number of args
-
-	path = os.Args[2]
+	path := os.Args[2]
 
 	switch os.Args[1] {
 
@@ -56,7 +40,7 @@ func main() {
 
 			info, _ := file.Info()
 
-			fmt.Println(info.Mode().Perm().String(), info.Size(), formatModTime(info.ModTime().String()), info.Name())
+			fmt.Println(info.Mode().Perm().String(), info.Size(), info.ModTime().Local().Format(time.ANSIC), info.Name())
 		}
 
 	default:
